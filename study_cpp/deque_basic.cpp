0dc1c8d132f1d5e3418b2b61533c553355e2129d
Soyoung <ma2214191@gmail.com>
2021-05-09 23:25:40 +0900

    "deque_basic"

---
+++ study_cpp/deque_basic.cpp
@@ -0,0 +1,18 @@
+#include <deque>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void) {
+	deque<int> dq;
+
+	for (int i = 1; i < 5; i++){
+		dq.push_back(i);
+	}
+	for (int i = 5; i < 10; i++) {
+		dq.push_front(i);
+	}
+	return 0;
+}
+
+// https://blockdmask.tistory.com/73
